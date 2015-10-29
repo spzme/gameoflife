c284eb93534ac68cc8681296abaf37c8c11185ff
spzme <fgletharg@gmail.com>
2015-10-29 15:11:29 +0100

    Revert "Improved simulation speed"
    This reverts commit 425d61f3f7a16b593ed3b314d258183ffe4d2c21.

--- src/controller/Game.java
+++ src/controller/Game.java
@@ -1,15 +1,11 @@
 package controller;
 
-import java.util.ArrayList;
-
 import model.*;
 
 public class Game {
 	private Field field;
 	private Field previousField;
 	private int generationCount;
-	ArrayList<Tuple> differences;
-	
 	
 	public Game(Field field) {
 		this.field = field;
@@ -18,14 +14,12 @@ public class Game {
 	}
 
 	public Field nextGeneration() {
-		differences = new ArrayList<Tuple>();
 		generationCount++;
 		previousField = field;
 		Field newField = new Field(field.getRowCount(), field.getColumnCount(), field.getGUI());
 		for (int i = 0; i < field.getRowCount(); i++) {
 			for (int j = 0; j < field.getColumnCount(); j++) {
-				boolean previousState = field.getAliveState(i, j);
-				boolean newState = false;
+				newField.insertCellAt(i, j, new Cell(field.getGUI()));
 				int neighbours = 0;
 				// top row
 				if (i > 0) {
@@ -75,27 +69,21 @@ public class Game {
 				if (neighbours < 2) {
 					// Die because of underpopulation.
 					newField.setDead(i, j);
-					newState = false;
 				}
 				if (neighbours == 3) {
 					// Live on to the next generation.
 					// Or, become alive!
 					newField.setAlive(i, j);
-					newState = true;
 				}
 				if (neighbours == 2 && field.getAliveState(i, j)) {
 					// Live on to the next generation
 					newField.setAlive(i, j);
-					newState = true;
 				}
 				if (neighbours > 3) {
 					// Die because of overpopulation
 					newField.setDead(i, j);
-					newState = false;
 				}
-				if (newState != previousState){
-					differences.add(new Tuple(i,j));
-				}
+
 			}
 		}
 		field = newField;
@@ -114,16 +102,6 @@ public class Game {
 	
 	public int getGenerationCount() {
 		return generationCount;
-	}
-	
-	public Tuple[] getDifferences(){
-		Tuple[] diff = new Tuple[differences.size()];
-		int i = 0;
-		for(Tuple cellDiff : differences){
-			diff[i] = cellDiff;
-			i++;
-		}
-		return diff;
 	}
 
 }
--- src/model/Field.java
+++ src/model/Field.java
@@ -3,46 +3,37 @@ package model;
 import views.NewGUI;
 
 public class Field {
-	private boolean[][] alive;
-	private boolean[][] hasLived;
+	private Cell[][] cells;
 	private int rows;
 	private int columns;
 	private NewGUI newGUI;
-	private int aliveCellCount;
 	
 	public Field(int rows, int columns, NewGUI newGUI) {
 		this.rows = rows;
 		this.columns = columns;
-		alive = new boolean[rows][columns];
-		hasLived = new boolean[rows][columns];
+		cells = new Cell[columns][rows];
 		this.newGUI = newGUI;
+		for(int m=0; m < rows; m++){
+			for(int n=0; n < columns; n++){
+				cells[n][m] = new Cell(newGUI);
+			}
+		}
 	}
 	
-	public void setAlive(int x, int y, boolean live){
-		if(live){
-			setAlive(x,y);
-		} else {
-			setDead(x,y);
-		}
+	public void setAlive(int x, int y, boolean alive) {
+		cells[x][y].setAlive(alive);
 	}
 	
 	public void setAlive(int x, int y){
-		if (alive[x][y] == false){
-			aliveCellCount++;
-		}
-		alive[x][y] = true;
-		hasLived[x][y] = true;
+		cells[x][y].setAlive();
 	}
 	
 	public void setDead(int x, int y){
-		if(alive[x][y]==true){
-			aliveCellCount--;
-		}
-		alive[x][y] = false;
+		cells[x][y].setDead();
 	}
 	
 	public boolean getAliveState(int x, int y){
-		return alive[x][y];
+		return cells[x][y].isAlive();
 	}
 	
 	public int getRowCount() {
@@ -57,18 +48,18 @@ public class Field {
 		StringBuilder sb = new StringBuilder();
 		for (int m = 0; m < rows; m++){
 			for (int n = 0; n < columns; n++){
-				sb.append(alive[m][n] ? "1" : "0");
+				sb.append(cells[n][m].isAlive() ? "1" : "0");
 			}
 			sb.append("\n");
 		}
 		System.out.println(sb);
 	}
 	
-	public NewGUI getGUI(){
-		return this.newGUI;
+	public void insertCellAt(int x, int y, Cell c){
+		cells[x][y] = c;
 	}
 	
-	public int getAliveCellCount(){
-		return aliveCellCount;
+	public NewGUI getGUI(){
+		return this.newGUI;
 	}
 }
